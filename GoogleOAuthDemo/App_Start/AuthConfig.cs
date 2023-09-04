981bb12f86354bae8a40a6c77ae7cc0951728929
PallaviKatari <pallavikatari@gmail.com>
2023-09-04 12:30:23 +0530

    Add project files.

---
+++ GoogleOAuthDemo/App_Start/AuthConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotNetOpenAuth.GoogleOAuth2;
+using Microsoft.AspNet.Membership.OpenAuth;
+
+namespace GoogleOAuthDemo
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            GoogleOAuth2Client clientGoog = new GoogleOAuth2Client("303346449770-56juh8bkbe3pbghj8mu21ek9c82jajh4.apps.googleusercontent.com", "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");
+            IDictionary<string, string> extraData = new Dictionary<string, string>();            
+            OpenAuth.AuthenticationClients.Add("google", () => clientGoog, extraData);
+        }
+    }
+}
---
+++ GoogleOAuthDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoogleOAuthDemo.MvcApplication" Language="C#" %>
